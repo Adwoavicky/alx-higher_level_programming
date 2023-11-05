d21eca9d7d1a6c7d3b80f200eff5fc51069dd298
Adwoavicky <veny78@ymail.com>
2023-11-05 11:30:31 -0800

    Matrix

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for elemnt in my_string:
+        if elemnts != "c" and elemnts != "C":
+            new_string += elements
+        return new_string
