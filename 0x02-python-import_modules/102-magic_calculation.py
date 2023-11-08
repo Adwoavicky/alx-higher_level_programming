05e860201693dfff271e3e3c4c3257d37a3ad66c
Adwoavicky <veny78@ymail.com>
2023-11-08 11:50:32 -0800

    Run

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
 def magic_calculation(a, b):
-    from magic_calculation_102 import add,
-    sub
     if a < b:
         c = add(a, b)
         for k in range(4, 6):
-            c = addc, k)
-            return c
-        else:
-            return (sub(a, b))
-
+            c = add(c, k)
+        return c
+    else:
+        return (sub(a, b))
