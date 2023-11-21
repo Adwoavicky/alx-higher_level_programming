f4f9f28c5333bd205b22bea8bed947c83ec32fd2
Adwoavicky <veny78@ymail.com>
2023-11-20 16:43:56 -0800

    Bytei

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,8 +4,8 @@ def magic_calculation(a, b):
     for k in range(1, 3):
         try:
             if k > a:
-                raise Exception ('Too far')
-                answer += a ** b / k
+                raise Exception('Too far')
+            answer += a ** b / k
         except Exception:
             answer = b + a
             break
